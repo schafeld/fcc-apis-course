7d49940e93195cd30874100b19faba1a2753e41e
Oliver Schafeld <oliver@schafeld.com>
2018-11-02 13:11:38 +0000

    add simple time server (route /now)

--- myApp.js
+++ myApp.js
@@ -35,7 +35,12 @@ app.use( express.static('public') );
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({"time": req.time});
+})
 
 /** 9)  Get input from client - Route parameters */
 
