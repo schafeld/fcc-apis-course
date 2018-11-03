f79e44c5e31cb655bc864507cfe294ebf8e8ee60
Oliver Schafeld <oliver@schafeld.com>
2018-11-03 13:46:48 +0000

    mount body-parser package

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
 app.use( function(req, res, next) {
@@ -10,7 +11,7 @@ app.use( function(req, res, next) {
 );
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use( bodyParser.urlencoded({extended: false}) );
 
 /** 3) Serve an HTML file */
 app.get('/', function (req, res) {
